78d29991d0aeeb580638e8445735ed343c819ca2
zhanruihan <Ruihan_zhan_hz@163.com>
2023-03-21 20:15:22 +0800

    数据预处理

--- data_processing/main.py
+++ data_processing/main.py
@@ -4,53 +4,10 @@ import numpy as np
 import os
 import datetime,time
 from pandas import read_csv
+from functools import reduce
 #显示所有列
 pd.set_option('display.max_columns',None)
-root = './data'
-file_names = os.listdir(root)
-df_list=[]
 
-#print(file_names) 数组
-def timestamp_to_time(timestamp):
-    # 转换成localtime
-    time_local =time.localtime(int(timestamp)/1000)
-    # 转换成新的时间格式(精确到秒)
-    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
-    return dt
-
-
-
-for file_name in file_names:
-    fileob = root + '/' + file_name
-    filename_info=os.path.splitext(file_name)[0].split('_')
-    #去除数据项id开头字母
-    filename_info[2]=filename_info[2].strip("k")
-
-    #filename_info:['070116M02', '13', 'k18069', 'avg']
-
-    with open(fileob, 'r', encoding='utf-8') as f:
-        content = json.load(f)
-        json_file_to_df = pd.DataFrame(pd.Series(content), columns=['data_item_value']).reset_index().rename(
-            columns={'index': 'timestamp'})
-        # print(type(content)) 类型为字典
-        # 转化为时间戳
-        json_file_to_df['timestamp']=json_file_to_df['timestamp'].apply(timestamp_to_time)
-        #插入列
-        json_file_to_df.insert(loc=1,column='deviceCode',value=np.repeat(filename_info[0],json_file_to_df.shape[0]))
-        json_file_to_df.insert(loc=2,column='point_name',value=np.repeat(filename_info[1],json_file_to_df.shape[0]))
-        json_file_to_df.insert(loc=3,column='kpi_id',value=np.repeat(filename_info[2],json_file_to_df.shape[0]))
-        json_file_to_df.insert(loc=4,column='polymerization_mode',value=np.repeat(filename_info[3],json_file_to_df.shape[0]))
-        # print(json_file_to_df)
-        df_list.append(json_file_to_df)
-df_json=pd.concat(df_list)
-# print(pd.concat(df_list))
-# 所有json文件拼接成一个df形状为[63394 rows x 6 columns]
-#               timestamp deviceCode point_name kpi_id  \
-# 0   2022-10-25 08:00:00  010420M01         01      1241017
-# 1   2022-10-26 08:00:00  010420M01         01      1241017
-# 2   2022-10-27 08:00:00  010420M01         01      1241017
-# 3   2022-10-28 08:00:00  010420M01         01      1241017
-# 4   2022-10-29 08:00:00  010420M01         01      1241017
 
 df_kpi = read_csv('./zconfs_bgtg.csv', header=0, index_col=False,encoding='ANSI', dtype = {'kpi_id' : str},names=['deviceCode','point_name','kpi_id','kpi_name'])
 df_kpi['point_name']=df_kpi['point_name'].apply(lambda x:str(x).zfill(2))
@@ -63,7 +20,71 @@ df_kpi['point_name']=df_kpi['point_name'].apply(lambda x:str(x).zfill(2))
 # 2      150208M02           03  1241013.0   高频加速度峭度
 # 3      150402M01           09  1241012.0  高频加速度RMS
 
-#合并两个表
-df_merged=pd.merge(df_json, df_kpi, on=['deviceCode','point_name','kpi_id'], how='left')
-df_merged.to_csv('./result.csv',encoding="utf_8_sig",mode="w")
-# print(df_merged)因为剔除了kpi项所以有两万多空的数据
+
+"""
+转化为时间戳格式
+"""
+def timestamp_to_time(timestamp):
+    # 转换成localtime
+    time_local =time.localtime(int(timestamp)/1000)
+    # 转换成新的时间格式(精确到秒)
+    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+    return dt
+"""
+查kpi_name
+"""
+def get_kpi_name(filename_info):
+
+    content=df_kpi[(df_kpi['deviceCode']==filename_info[0]) & (df_kpi['point_name']==filename_info[1]) & (df_kpi['kpi_id']==filename_info[2])]
+
+    if(len(content['kpi_name'])):
+        return content['kpi_name'].values[0]
+    else:
+        print('没有该kpi_name')
+def merge_df_list(df_list,key):
+    df_merge = reduce(lambda left, right: pd.merge(left, right, on=[key], how='left'), df_list)
+    return df_merge
+def get_devices_df(path,device_code,point_name,method='avg'):
+    """
+
+    :param path: 文件路径
+    :param device_code: 设备编码
+    :param point_name: 测点编号
+    :return: timestamp  高频加速度有效值 ...
+
+    """
+    file_names = os.listdir(path)
+    df_list = []
+    for file_name in file_names:
+        fileob = path + '/' + file_name
+        filename_info=os.path.splitext(file_name)[0].split('_')
+        #去除数据项id开头字母
+        filename_info[2]=filename_info[2].strip("k")
+
+        #filename_info:['070116M02', '13', 'k18069', 'avg']
+
+
+        if((filename_info[0]==device_code) & (filename_info[1]==point_name) & (filename_info[3]=='avg')):
+            kpi_name = get_kpi_name(filename_info)
+            with open(fileob, 'r', encoding='utf-8') as f:
+                content = json.load(f)
+                json_file_to_df = pd.DataFrame(pd.Series(content), columns=[locals()['kpi_name']]).reset_index().rename(
+                    columns={'index': 'timestamp'})
+                # 转化为时间戳
+                json_file_to_df['timestamp']=json_file_to_df['timestamp'].apply(timestamp_to_time)
+
+                df_list.append(json_file_to_df)
+    if len(df_list):
+        return merge_df_list(df_list,'timestamp')
+    else:
+        print('没有kpi数据')
+
+if __name__ == '__main__':
+    root = './data'
+    #070116M02,010221M01
+    device1=get_devices_df(root,'070116M02','01')
+    device2=get_devices_df(root,'010223M01','01')
+    device3=get_devices_df(root,'170201M06','01')
+    devices_df=merge_df_list([device1,device2,device3],'timestamp')
+    devices_df.to_csv('./result.csv', encoding="utf_8_sig", mode="w")
+    # print(devices_df)
