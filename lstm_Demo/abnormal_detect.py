d913a91578147f68edc12d459c298cb59fcf9f15
zhanruihan <Ruihan_zhan_hz@163.com>
2024-01-01 17:15:42 +0800

    异常指标暂存

--- lstm_Demo/abnormal_detect.py
+++ lstm_Demo/abnormal_detect.py
@@ -22,7 +22,9 @@ def find_anomalies(random_data):
     # 异常
     for i in range(len(random_data)):
 
-        if random_data[i] > upper_limit or random_data[i] < lower_limit:
+        # if random_data[i] > upper_limit or random_data[i] < lower_limit:
+        #温度下限不考虑
+        if random_data[i] > upper_limit :
             x_values.append(i)
             # y_values.append(random_data[i])
 
@@ -37,6 +39,33 @@ def detect_outline(y, y_hat):
     plot_outline(x_values, y_values)
 
 
-
 def plot_outline(x_values, y_values):
     plt.scatter(x_values, y_values, s=10,c='none',edgecolors='r')
+
+#异常监测的指标
+def cal_score(pre_result, obs_result):
+    """
+    :param pre_result: 预测结果
+    :param obs_result: 实际结果
+    :return: 分数值
+    """
+    res = (pre_result ^ obs_result)  # 异或使得判断正确的为0,判断错误的为1
+    r = np.bincount(res)
+    tp_list = ((pre_result) & (obs_result))  # 实际为真，预测为真，真阳性
+    fp_list = (pre_result & (~obs_result))  # 实际为假，预测为真,假阳性
+    tp_list = tp_list.tolist()
+    fp_list = fp_list.tolist()
+    tp = tp_list.count(1)
+    fp = fp_list.count(1)
+    tn = r[0] - tp
+    if len(r) == 1:  # 判断全对的情况下
+        fn = 0
+    else:
+        fn = r[1] - fp
+    p = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    F1 = (2 * tp) / (2 * tp + fn + fp)
+    acc = (tp + tn) / (tp + tn + fp + fn)
+    print("tp=%d,  tn=%d, fp=%d, fn=%d" % (tp, tn, fp, fn))
+    return p, recall, F1, acc
+
